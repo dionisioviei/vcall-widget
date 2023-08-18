bff44fa4e14d1994a387d349e89c7a224957aa35
Dionisio Vieira <dionisioviei@gmail.com>
2023-08-18 16:04:30 -0300

    test: add shadow root

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,19 @@ if (!document.getElementById('vittelgroup-vcall')) {
   const div = document.createElement('div')
   div.id = 'vittelgroup-vcall'
   document.body.appendChild(div)
-}
+  const shadowRoot = div.attachShadow({ mode: 'open' })
+  const NOOP = () => {}
+  shadowRoot.removeAttribute = NOOP
+  shadowRoot.setAttribute = NOOP
+  fetch('https://cdn.jsdelivr.net/gh/dionisioviei/vcall-widget@master/dist/style.css')
+    .then((response) => response.text())
+    .then((styles) => {
+      const style = document.createElement('style')
+      style.textContent = styles
+      shadowRoot?.appendChild(style)
+    })
 
-createApp(App).mount('#vittelgroup-vcall')
+  createApp(App).mount(shadowRoot)
+} else {
+  createApp(App).mount('#vittelgroup-vcall')
+}
