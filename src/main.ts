6158e1d8950dfd55ad70679ff4537baad1ea3180
Dionisio Vieira <dionisioviei@gmail.com>
2023-08-21 12:34:18 -0300

    fix: remove shadow dom

--- src/main.ts
+++ src/main.ts
@@ -4,23 +4,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 if (!document.getElementById('vittelgroup-vcall')) {
-  const div = document.createElement('div')
-  div.id = 'vittelgroup-vcall'
   const app = document.createElement('div')
-  app.id = 'vittelgroup-app'
+  app.id = 'vittelgroup-vcall'
   document.body.appendChild(app)
-
-  app.attachShadow({ mode: 'open' }).appendChild(div)
-
-  fetch('https://cdn.jsdelivr.net/gh/dionisioviei/vcall-widget@master/dist/style.css')
-    .then((response) => response.text())
-    .then((styles) => {
-      const style = document.createElement('style')
-      style.textContent = styles
-      div.appendChild(style)
-    })
-
-  createApp(App).mount(div)
-} else {
-  createApp(App).mount('#vittelgroup-vcall')
 }
+createApp(App).mount('#vittelgroup-vcall')
