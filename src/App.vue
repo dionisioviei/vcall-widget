08017c78cde81a59f2bbc97213b67060e85fd94a
Dionisio Vieira <dionisioviei@gmail.com>
2023-12-28 17:54:24 -0300

    remove test code

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,5 @@ import VCallWidget from './components/VCallWidget.vue'
 </script>
 
 <template>
-  <li>
-    <ul><a href="tel:+5511992242283" class="tw-text-slate-700">+5511992242283</a></ul>
-    <ul><a href="tel:5511992242283" class="tw-text-slate-700">5511992242283</a></ul>
-    <ul><a href="tel:11992242283" class="tw-text-slate-700">11992242283</a></ul>
-    <ul><a href="tel:011992242283" class="tw-text-slate-700">011992242283</a></ul>
-    <ul><a href="tel:+111992242283" class="tw-text-slate-700">+111992242283</a></ul>
-    <ul><a href="tel:992242283" class="tw-text-slate-700">992242283</a></ul>
-  </li>
   <VCallWidget />
 </template>
